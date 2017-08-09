e5e96de707318477a5deecf1c1c66890c8b6cf91
Andrea0229 <cpy123go@sina.cn>
2017-08-09 10:57:39 +0800

    Update main.js

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -5,11 +5,8 @@
 function printReceipt(tags){
   var resultStr = '***<没钱赚商店>收据***\n';
   var allItems = loadAllItems();              //获取商品列表
-//  console.log(allItems);
   var allDiscount = loadPromotions();         //获取打折信息
-
   var allGoods = getPayoff(allItems, allDiscount, tags);      //计算所有商品价格
-//  console.log(allDiscount);
 
 }
 
@@ -33,7 +30,7 @@ function getPayoff(allItems, allDiscount, tags){
     goods = getOriginPrice(allItems, goods); //获取名称单，价，单位，
     // console.log('goods.barcode='+goods.barcode+'\t goods.count='+goods.count+'\tname:'+goods.name+'\tprice:'+goods.price);
     goods = getActualPrice(allDiscount, goods);  //获取商品折扣，商品小计
-    result += '名称：'+goods.name+'，数量：'+goods.count+goods.unit+'，单价：'+goods.price+'，小计：'+goods.subtotal.toFixed(2)+'(元)\n';
+    result += '名称：'+goods.name+'，数量：'+goods.count+goods.unit+'，单价：'+goods.price+'(元)，小计：'+goods.subtotal.toFixed(2)+'(元)\n';
     all_price += goods.subtotal;
     all_discount += goods.offer;
   }
@@ -132,77 +129,3 @@ function getDiscount(allDiscount, goods_barcode){
 
   return 'default';
 }
-
-
-/*
-var loadAllItems  = require('../test/fixtures.js');
-const loadPromotions  = require('../test/fixtures.js');
-
-function printReceipt(tags) {
-  var resultStr = '***<没钱赚商店>收据***\n';
-  var items = loadAllItems();
-  var promotions = loadPromotions();
-
-  var result = getAccountBill(tags, items, promotions);
-
-  var sum = 0;
-  var discount = 0;
-  for (let i = 0; i < items.length; i++) {
-    if (result.hasOwnProperty(items[i].barcode)) {
-      var price = calculatePromotions(result[items[i].barcode].promotion, result[items[i].barcode].num, items[i].price);
-      resultStr += '名称：' + items[i].name + '，数量：' + result[items[i].barcode].num + items[i].unit + '，单价：' + items[i].price.toFixed(2) + '(元)，小计：' + price.totalPrice.toFixed(2) + '(元)\n';
-      sum += price.totalPrice;
-      discount += price.discount;
-    }
-  }
-
-  resultStr += '----------------------\n总计：' + sum.toFixed(2) + '(元)\n节省：' + discount.toFixed(2) + '(元)\n**********************';
-  console.log(resultStr);
-}
-
-function getAccountBill(tags, items, promotions) {
-  var result = new Array();
-  for (var i = 0; i < tags.length; i++) {
-    var currentItem = tags[i];
-    var num = 1;
-    if (currentItem.indexOf('-') >= 0) {
-      var arr = currentItem.split('-');
-      currentItem = arr[0];
-      num = arr[1];
-    }
-
-
-    if (!result.hasOwnProperty(currentItem)) {
-      var promotion = 'default';
-      for (var j = 0; j < promotions.length; j++) {
-        if (promotions[j].barcodes.indexOf(currentItem) > -1) {
-          result[currentItem] = {
-            'num': parseFloat(num),
-            'promotion': promotions[j].type
-          };
-        }
-      }
-    } else {
-      result[currentItem].num += parseFloat(num);
-    }
-  }
-  return result;
-}
-
-function calculatePromotions(promotion, num, price) {
-  var res = {
-    'totalPrice' : 0,
-    'discount' : 0
-  };
-  switch (promotion) {
-    case 'BUY_TWO_GET_ONE_FREE' :
-      res['totalPrice'] = (parseInt(num / 3) * 2 + num % 3) * price;
-      res['discount'] = parseInt(num / 3) * price;
-      break;
-    default :
-      res['totalPrice'] = price * num;
-      break;
-  }
-  return res;
-}
-*/
